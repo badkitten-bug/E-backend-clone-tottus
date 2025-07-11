d228cf2e90a3017d591d56e4ce395b3a270443d4
Steve Gomez <104510289+badkitten-bug@users.noreply.github.com>
2025-07-11 12:01:11 -0500

    Actualiza la configuración del middleware CORS para corregir el origen permitido, eliminando la barra final del dominio de producción y mejorando la claridad en la gestión de solicitudes.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -6,8 +6,8 @@ export default [
     name: 'strapi::cors',
     config: {
       origin: [
+        'https://frontend-clone-totus-git-master-steves-projects-9398f8a1.vercel.app', // tu dominio de producción
         'http://localhost:3000', // para desarrollo local
-        'https://frontend-clone-totus-git-master-steves-projects-9398f8a1.vercel.app/', // tu dominio de producción
         // agrega aquí otros dominios si los necesitas
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
